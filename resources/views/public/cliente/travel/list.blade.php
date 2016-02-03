d95d26ca7c01fb2237e04784eaf37cf57a3c5f25
hugo <hugomorelli2@gmail.com>
2016-02-03 16:52:03 -0430

    TimeLine

--- resources/views/public/cliente/travel/list.blade.php
+++ resources/views/public/cliente/travel/list.blade.php
@@ -1,43 +1,53 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="row">
+
+
+    <ul class="timeline">
         @foreach( $data as $viajes)
 
         @foreach( $viajes as $viaje)
-        <div class="col-md-4">
-    <a class="travel-a" href="{{ URL::to('travels/'.$viaje->paquete->cod) }}">
-            <!-- Widget: user widget style 1 -->
-            <div class="box box-widget widget-user travel ">
-                <!-- Add the bg color to the header using any of the bg-* classes -->
-                <div class="widget-user-header bg-black cover-viaje">
-                </div>
-                <div class="box-footer">
+                <!-- timeline time label -->
+        <li class="time-label">
+        <span class="bg-red">
+            10 Feb. 2014
+        </span>
+        </li>
+        <!-- /.timeline-label -->
+
+        <!-- timeline item -->
+        <li>
+            <!-- timeline icon -->
+            <i class="glyphicon glyphicon-globe bg-blue"></i>
+            <div class="timeline-item">
+                <div class="timeline-body">
                     <div class="row">
-                        <div class="col-sm-8 border-right">
-                            <div class="description-block">
-                                <h5 class="description-header">{{$viaje->paquete->titulo}}</h5>
-                            </div>
-                            <!-- /.description-block -->
-                        </div>
-
-                        <!-- /.col -->
-                        <div class="col-sm-4">
-                            <div class="description-block">
-                                <h5 class="description-header">{{$viaje->paquete->fecha}}</h5>
-                            </div>
-                            <!-- /.description-block -->
-                        </div>
-                        <!-- /.col -->
+                    <div class="col-md-4">
+                        <a class="travel-a" href="{{ URL::to('travels/'.$viaje->paquete->cod) }}">
+                            <!-- Widget: user widget style 1 -->
+                            <div class="box box-widget widget-user travel ">
+                                <!-- Add the bg color to the header using any of the bg-* classes -->
+                                <div class="widget-user-header bg-black cover-viaje">
+                                    <h3 class="widget-user-username">{{$viaje->paquete->titulo}}</h3>
+                                    <h5 class="widget-user-desc">{{$viaje->paquete->fecha}}</h5>
+                                </div>
+                             </div>
+                        </a>
+                        <!-- /.widget-user -->
                     </div>
-                    <!-- /.row -->
+                    </div>
                 </div>
-            </div>
-    </a>
 
-            <!-- /.widget-user -->
-        </div>
+
+            </div>
+        </li>
         @endforeach
         @endforeach
-    </div>
+                <!-- END timeline item -->
+        <li>
+            <i class="fa fa-clock-o bg-gray"></i>
+        </li>
+
+    </ul>
+
 @endsection
